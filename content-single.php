7bb970a4959cc4d99b4c2eabf4eebcba807ac2fd
Eduardo Zulian <eduardozulian@gmail.com>
2013-11-13 16:26:24 -0200

    Adiciona permalink ao título das singulars

--- content-single.php
+++ content-single.php
@@ -6,17 +6,22 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<div class="entry-meta entry-category">
+			<?php the_category(' '); ?>
+		</div><!-- .entry-meta -->
+
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
 		<div class="entry-meta">
 			<?php kaingang_posted_on(); ?>
 		</div><!-- .entry-meta -->
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="entry-image">
-				<?php the_post_thumbnail( 'feature-singular' ); ?>
-			</div><!-- .entry-image -->
-		<?php endif ?>
 	</header><!-- .entry-header -->
+
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="entry-image">
+			<?php the_post_thumbnail( 'feature-singular' ); ?>
+		</div><!-- .entry-image -->
+	<?php endif ?>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
@@ -30,36 +35,11 @@
 
 	<footer class="entry-meta">
 		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'kaingang' ) );
+		/* translators: used between list items, there is a space after the comma */
+		$tag_list = get_the_tag_list( '', __( ', ', 'kaingang' ) );
 
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', __( ', ', 'kaingang' ) );
-
-			if ( ! kaingang_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'kaingang' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'kaingang' );
-				}
-
-			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'kaingang' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'kaingang' );
-				}
-
-			} // end check for categories on this blog
-
-			printf(
-				$meta_text,
-				$category_list,
-				$tag_list,
-				get_permalink()
-			);
+		if ( ! empty( $tag_list ) )
+			echo '<span class="meta-title">' . __( 'Tags', 'kaingang' ) . $tag_list;
 		?>
 
 		<?php edit_post_link( __( 'Edit', 'kaingang' ), '<span class="edit-link">', '</span>' ); ?>
