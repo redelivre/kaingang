ef861e9a34f520704a7814000bd672cf1f0368ee
Eduardo Zulian <eduardozulian@gmail.com>
2013-11-13 13:03:41 -0200

    Adiciona a opção para upload de logo

--- header.php
+++ header.php
@@ -2,20 +2,20 @@
 /**
  * The Header for our theme.
  *
- * Displays all of the <head> section and everything up till <div id="content">
+ * Displays all of the <head> section and everything up till <div id="content" class="site-content wrap">
  *
  * @package Kaingang
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -24,6 +24,14 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="wrap">
 			<div class="site-branding">
+				<?php
+				// Check if there's a custom logo
+                $logo = get_theme_mod( 'kaingang_logo' );
+                if ( isset( $logo ) && ! empty( $logo ) ) : ?>
+	                <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+	                    <img class="site-logo" src="<?php echo $logo; ?>" alt="Logo <?php bloginfo ( 'name' ); ?>" />
+	                </a>
+            	<?php endif; ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</div>
--- inc/customizer.php
+++ inc/customizer.php
@@ -16,6 +16,58 @@
  */
 function kaingang_customize_register( $wp_customize ) {
 
+
+	/**
+	 * Customize Image Reloaded Class
+	 *
+	 * Extend WP_Customize_Image_Control allowing access to uploads made within
+	 * the same context
+	 * 
+	 */
+	class My_Customize_Image_Reloaded_Control extends WP_Customize_Image_Control {
+		/**
+		 * Constructor.
+		 *
+		 * @since 3.4.0
+		 * @uses WP_Customize_Image_Control::__construct()
+		 *
+		 * @param WP_Customize_Manager $manager
+		 */
+		public function __construct( $manager, $id, $args = array() ) {
+		
+		parent::__construct( $manager, $id, $args );
+		       
+		}
+		
+		/**
+		* Search for images within the defined context
+		* If there's no context, it'll bring all images from the library
+		* 
+		*/
+		public function tab_uploaded() {
+		$my_context_uploads = get_posts( array(
+		    'post_type'  => 'attachment',
+		    'meta_key'   => '_wp_attachment_context',
+		    'meta_value' => $this->context,
+		    'orderby'    => 'post_date',
+		    'nopaging'   => true,
+		) );
+		
+		?>
+		
+		<div class="uploaded-target"></div>
+		
+		<?php
+		if ( empty( $my_context_uploads ) )
+		    return;
+		
+		foreach ( (array) $my_context_uploads as $my_context_upload )
+		    $this->print_tab_image( esc_url_raw( $my_context_upload->guid ) );
+		}
+		
+	}
+
+
 	// Add postMessage support for site title and description for the Theme Customizer.
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
@@ -32,6 +84,24 @@ function kaingang_customize_register( $wp_customize ) {
 		'type'     => 'checkbox',
 		'settings' => 'kaingang_display_header_text'
 	) );
+
+	// Branding section
+	$wp_customize->add_section( 'kaingang_branding', array(
+		'title'    => __( 'Branding', 'kaingang' ),
+		'priority' => 30,
+	) );
+	
+	// Branding: Logo settings
+	$wp_customize->add_setting( 'kaingang_logo', array(
+		'capability'  => 'edit_theme_options',
+	) );
+	
+    $wp_customize->add_control( new My_Customize_Image_Reloaded_Control( $wp_customize, 'kaingang_logo', array(
+        'label'   	=> __( 'Logo', 'kaingang' ),
+        'section'	=> 'kaingang_branding',
+        'settings' 	=> 'kaingang_logo',
+        'context'	=> 'kaingang-custom-logo'
+    ) ) ); 
 
 
 }
@@ -60,7 +130,7 @@ function kaingang_customize_css() {
 		/* Header text */
 		.site-title,
 		.site-description {
-			clip: rect(1px, 1px, 1px, 1px);
+			clip: rect(1px, 1px, 1px, 1px) !important;
 			position: absolute !important;
 		}
 		<?php endif; ?>
