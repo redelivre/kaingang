d040aa8514081e53074266672f76057f2a8ffb34
Eduardo Zulian <eduardozulian@gmail.com>
2013-11-25 11:42:11 -0200

    Adiciona categorias ao rodapé do post

--- content-single.php
+++ content-single.php
@@ -6,12 +6,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<div class="entry-meta entry-category">
-			<?php the_category(' '); ?>
-		</div><!-- .entry-meta -->
-
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-
 		<div class="entry-meta">
 			<?php kaingang_posted_on(); ?>
 		</div><!-- .entry-meta -->
@@ -33,15 +28,26 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-meta">
+	<footer class="entry-meta entry-meta--footer">
 		<?php
+		/* translators: used between list items, there is a space after the comma */
+        $category_list = get_the_category_list( __( ', ', 'kaingang' ) );
+
 		/* translators: used between list items, there is a space after the comma */
 		$tag_list = get_the_tag_list( '', __( ', ', 'kaingang' ) );
 
-		if ( ! empty( $tag_list ) )
-			echo '<span class="meta-title">' . __( 'Tags', 'kaingang' ) . $tag_list;
-		?>
+		if ( ! empty( $tag_list ) ) : ?>
+			<div class="entry-meta__tag">
+				<?php echo '<span class="meta-title">' . __( 'Tags', 'kaingang' ) . '</span>' . $tag_list; ?>
+			</div>
+		<?php
+		endif;
 
+		if ( ! empty( $category_list ) ) : ?>
+			<div class="entry-meta__category">
+				<?php echo '<span class="meta-title">' . __( 'Categories', 'kaingang' ) . '</span>' . $category_list; ?>
+			</div>
+		<?php endif; ?>
 		<?php edit_post_link( __( 'Edit', 'kaingang' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
