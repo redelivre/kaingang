de580cce268cd90ae8a02c2fb193291c3c7545f2
Eduardo Zulian <eduardozulian@gmail.com>
2013-11-27 12:27:06 -0200

    Define a estrutura para o container dos archives

--- content.php
+++ content.php
@@ -4,23 +4,25 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'clear' ); ?>>
 	<?php if ( has_post_thumbnail() ) : ?>
 		<div class="entry-image">
-			<?php the_post_thumbnail( 'feature-archive' ); ?>
+			<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'feature-archive' ); ?></a>
 		</div><!-- .entry-image -->
 	<?php endif ?>
-	<header class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+	<div class="entry-body">
+		<header class="entry-header">
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
-		<?php if ( $post->post_type == 'post' ) : ?>
-		<div class="entry-meta">
-			<?php kaingang_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+			<?php if ( $post->post_type == 'post' ) : ?>
+			<div class="entry-meta">
+				<?php kaingang_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+	</div><!-- .entry-body -->
 </article><!-- #post-## -->
