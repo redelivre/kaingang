00d2ccff3751c48ec2a6f423a3904925b60737c2
Eduardo Zulian <eduardozulian@gmail.com>
2014-01-18 16:11:14 -0200

    Simplifica os templates page e single

--- page.php
+++ page.php
@@ -17,7 +17,28 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+						<?php edit_post_link( __( 'Edit', 'kaingang' ), '<span class="edit-link">', '</span>' ); ?>
+					</header><!-- .entry-header -->
+
+					<?php if ( has_post_thumbnail() ) : ?>
+						<div class="entry-image">
+							<?php the_post_thumbnail( 'feature-singular' ); ?>
+						</div><!-- .entry-image -->
+					<?php endif ?>
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . __( 'Pages:', 'kaingang' ),
+								'after'  => '</div>',
+							) );
+						?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-## -->
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
