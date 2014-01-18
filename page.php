1f39cca1030b5f31552df0747b0b7d60f1d473ef
Eduardo Zulian <eduardozulian@gmail.com>
2014-01-18 16:15:25 -0200

    Remove a área de comentários das páginas

--- page.php
+++ page.php
@@ -40,12 +40,6 @@ get_header(); ?>
 					</div><!-- .entry-content -->
 				</article><!-- #post-## -->
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() )
-						comments_template();
-				?>
-
 			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
